59fb3fd14b76b3e709361742d2babc409957d785
Johan Peraldi <johan.peraldi@gmail.com>
2022-09-05 17:33:32 +0200

    Fix tests relating to admin login and bill dates displayed in reverse chronological order

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,11 +1,10 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from './VerticalLayout.js';
+import ErrorPage from './ErrorPage.js';
+import LoadingPage from './LoadingPage.js';
 
-import Actions from './Actions.js'
+import Actions from './Actions.js';
 
-const row = (bill) => {
-  return (`
+const row = (bill) => (`
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,15 +15,11 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `);
 
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+const rows = (data) => ((data && data.length) ? data.map((bill) => row(bill)).join('') : '');
 
 export default ({ data: bills, loading, error }) => {
-  
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -40,14 +35,14 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `);
 
   if (loading) {
-    return LoadingPage()
-  } else if (error) {
-    return ErrorPage(error)
+    return LoadingPage();
+  } if (error) {
+    return ErrorPage(error);
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
@@ -76,5 +71,5 @@ export default ({ data: bills, loading, error }) => {
       </div>
       ${modal()}
     </div>`
-  )
-}
+  );
+};
